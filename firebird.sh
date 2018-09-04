a803ed9a4b658e87b8b77b773493849ddd8bcc1d
Marina Karpukhina <marina.ks.developer@gmail.com>
2018-09-04 19:54:14 +0300

    feat(firebird): install

---
+++ firebird.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -LO https://github.com/FirebirdSQL/firebird/releases/download/R3_0_3/Firebird-3.0.3-32900-x86_64.pkg
+open ./Firebird-3.0.3-32900-x86_64.pkg
+
+firebirdHome='export FIREBIRD_HOME="/Library/Frameworks/Firebird.framework/Resources"'
+grep -q -F "$firebirdHome" ~/.bash_profile || echo "$firebirdHome" >> ~/.bash_profile
+
+firebirdBin='export PATH=$PATH:$FIREBIRD_HOME/bin'
+grep -q -F "$firebirdBin" ~/.bash_profile || echo "$firebirdBin" >> ~/.bash_profile
