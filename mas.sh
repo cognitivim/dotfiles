33147d0ceed5c3be8dca46bb624233020fcd9c0d
Marina Karpukhina <marina.ks.developer@gmail.com>
2018-11-22 18:20:54 +0300

    feat(mas): pipifier

--- mas.sh
+++ mas.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-
-# App store app
-mas signin lllli.marina.illll@gmail.com
+# App store apps
 
 mas install 585829637  # Todoist
 mas install 937984704  # Amphetamine
-mas install 1440147259 # AdGuard for Safari
+
+# Safari extentions
+
+mas install 1440147259 # AdGuard
+mas install 1160374471 # PiPifier
