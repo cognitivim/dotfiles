4722179f0f2cf4e1c510f774987349f080dfdc94
Marina Karpukhina <marina.ks.developer@gmail.com>
2018-11-29 18:54:24 +0300

    chore(git): wip

--- git.sh
+++ git.sh
@@ -1,35 +1,31 @@
 git config --global user.name $GIT_USERNAME
 git config --global user.email $GIT_EMAIL
 
-# https://help.github.com/articles/generating-a-new-gpg-key/
+# SIGN KEY
 
-# gen
+# generate:
 gpg --full-generate-key
-# ...
-
-# gpg --list-secret-keys --keyid-format LONG
+gpg --list-secret-keys --keyid-format LONG
 # ... set $GPG_KEYID
 
-# git
+# setup git:
 git config --global user.signingkey $GPG_KEYID
 git config --global commit.gpgsign true
 # git config --global credential.helper osxkeychain
 # git config gpg.program gpg2
-# ?
 test -r ~/.bash_profile && echo 'export GPG_TTY=$(tty)' >> ~/.bash_profile
 echo 'export GPG_TTY=$(tty)' >> ~/.profile
 
-# github
+# setup github:
 gpg --armor --export $GPG_KEYID
+# ... add to github
 
-# agent
-# cat ~/.gnupg/gpg-agent.conf 
+# setup agent:
 echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
 echo "default-cache-ttl 86400" >> ~/.gnupg/gpg-agent.conf
 echo "max-cache-ttl 864000" >> ~/.gnupg/gpg-agent.conf
 echo "default-cache-ttl-ssh 86400" >> ~/.gnupg/gpg-agent.conf
 echo "max-cache-ttl-ssh 864000" >> ~/.gnupg/gpg-agent.conf
+killall gpg-agent
 
 # echo "default-key <keyid>" >> ~/.gnupg/gpg.conf
-
-killall gpg-agent
