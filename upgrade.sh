531ccbeb4cfd7a9ecc072789e8efeaa75e0d0146
Marina Karpukhina <marina.ks.developer@gmail.com>
2018-11-29 22:13:50 +0300

    feat(upgrade): mas

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## mas
+
+mas upgrade
+
+## brew
+
 brew update
 brew upgrade
 
@@ -7,10 +13,13 @@ brew cleanup
 brew prune
 brew doctor
 
+## brew cask
+
 brew cask upgrade
 brew cask doctor
 
-# update npm
-npm -g update
-npm update
-npm i -g npm
+## npm
+
+# npm -g update
+# npm update
+# npm i -g npm
