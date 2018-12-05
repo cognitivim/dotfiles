31a747347df4f2cce76a87eb192feee902325f13
Marina Karpukhina <marina.ks.developer@gmail.com>
2018-12-05 17:11:12 +0300

    wip

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 personal mac setup: dotfiles, preferences, apps
 
-
 ## Install
 
 ```sh
@@ -11,16 +10,13 @@ cd ~/Desktop/dotfiles
 curl --location https://github.com/punkusha/dotfiles/tarball/master | tar -xz --strip-components 1 --exclude={.editorconfig,.gitignore,README.md,LICENSE}
 ```
 
-* `Brewfile`: A list of command-line utils, apps, and others, that will be installed via [Homebrew](https://brew.sh/), [Homebrew-Cask](https://caskroom.github.io/), and [`mas-cli`](https://github.com/mas-cli/mas)
-
+- `Brewfile`: A list of command-line utils, apps, and others, that will be installed via [Homebrew](https://brew.sh/), [Homebrew-Cask](https://caskroom.github.io/), and [`mas-cli`](https://github.com/mas-cli/mas)
 
 > Don't blindly run my scripts. Use at your own risk!
-
 
 ## Credits
 
 Inspired by [Mathias’s dotfiles](https://github.com/mathiasbynens/dotfiles)
-
 
 ## License
 
--- scripts/modules/brew.sh
+++ scripts/modules/brew.sh
@@ -1,46 +1,29 @@
 #!/usr/bin/env bash
 
-# ask for the administrator password upfront
-# cache the root password
+# ask for the administrator password upfront, cache the root password
 sudo -v
-
 # keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-
-echo "Homebrew installation..." 
-
-
+echo "Homebrew installation..."
 if test ! $(which brew); then 
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
   # http://docs.brew.sh/Troubleshooting.html
   cd /usr/local && sudo chown -R $(whoami) bin etc include lib sbin share var Frameworks
 else
   echo "Brew is already installed!"
 fi
 
-brew tap homebrew/bundle
-
-
-echo "Updating Homebrew formulas ..."
-
-
-brew update &&
-# brew upgrade brew-cask &&
-brew cleanup &&
-# brew cask cleanup
-
-# check for issues. set permissions (prompting for sudo password) if necessary
-# brew cask doctor
+echo "Updating Homebrew formulas..."
+brew update
+brew cleanup
+brew cask cleanup
+brew cask doctor
 brew doctor
 
+brew tap homebrew/bundle
 
-echo "Installing all of the dependencies ..."
-
-
-# brew bundle -v --file=./Brewfile
-brew bundle
-
+echo "Installing all of the dependencies..."
+brew bundle -v --file=../../configs/Brewfile
 
 echo "Done."
