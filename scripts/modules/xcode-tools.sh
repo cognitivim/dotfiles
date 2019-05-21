0ea496b127e04fb862cd503635b65c2e78fcbecb
Marina Karpukhina <marina.ks.developer@gmail.com>
2019-05-22 01:39:31 +0300

    chore(xcode-tools): refactor

--- scripts/modules/xcode-tools.sh
+++ scripts/modules/xcode-tools.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
-echo "Installing Xcode CLI tools..."
+echo "[xcode-tools] install..."
+
 
 #if [[ ! -z "/Library/Developer/CommandLineTools/" ]]; then
   if ! $(xcode-select -p &>/dev/null); then
-
-    # See http://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line
+    # see http://apple.stackexchange.com/questions/107307/how-can-i-install-the-command-line-tools-completely-from-the-command-line
+    
     TOOLS_PLACEHOLDER='/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress'
     touch "${TOOLS_PLACEHOLDER}"
+    
     sleep 3
+    
     TOOLS="$(softwareupdate --list | sed -E '/^.*\*\ *(Command Line Tools.*)\ *$/h;g;$!d;s//\1/')"
     softwareupdate --verbose --install "${TOOLS}"
+    
     rm -f "${TOOLS_PLACEHOLDER}"
 
     # choosing command line tools as default    
@@ -18,16 +22,15 @@ echo "Installing Xcode CLI tools..."
 
     # accept the Xcode/iOS license agreement
     if ! $(sudo xcodebuild -license status); then
-      echo "License accepting..."
+      echo "[xcode-tools] license accepting..."
       sudo xcodebuild -license accept
     fi
-
-    echo "Done."
+    echo "[xcode-tools] done."
   else
-    echo "Xcode CLI tools is already installed!"
+    echo "[xcode-tools] Xcode CLI tools is already installed!"
   fi
 #else
 #  # choosing command line tools as default
 #  sudo xcode-select --switch /Library/Developer/CommandLineTools/
-#  echo "Done."
+#  echo "[xcode-tools] done."
 #fi
